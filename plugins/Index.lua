04dc0a86070c7dd905aa70524356c0d799468df9
mosavy <www.mortezamosavy94@gmail.com>
2016-03-26 13:02:27 +0430

    Create Index.lua

---
+++ plugins/Index.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return 'IndexTG Bot V2.1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot' 
+  
+end
+
+return {
+  patterns = {
+    "^#index$"
+  }, 
+  run = run 
+}
+
+end
